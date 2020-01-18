103c5c0c298d9e6a843f98351094592edf93b57a
Jake_Pollack4055 <Jake1905@icloud.com>
2020-01-17 18:58:35 -0800

    Updated variable names

--- src/main/java/frc/team2485/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/team2485/robot/Subsystems/Drivetrain.java
@@ -12,27 +12,27 @@ public class Drivetrain extends SubsystemBase {
     private DifferentialDrive drive;
 
 
-    private WL_SparkMax SparkLeft1Master;
-    private WL_SparkMax SparkLeft2;
-    private WL_SparkMax SparkLeft3;
+    private WL_SparkMax _sparkLeft1Master;
+    private WL_SparkMax _sparkLeft2;
+    private WL_SparkMax _sparkLeft3;
 
-    private WL_SparkMax SparkRight1Master;
-    private WL_SparkMax SparkRight2;
-    private WL_SparkMax SparkRight3;
+    private WL_SparkMax _sparkRight1Master;
+    private WL_SparkMax _sparkRight2;
+    private WL_SparkMax _sparkRight3;
 
     public Drivetrain() {
-        this.SparkLeft1Master = new WL_SparkMax(0);
-        this.SparkLeft2 = new WL_SparkMax(0);
-        this.SparkLeft3 = new WL_SparkMax(0);
+        this._sparkLeft1Master = new WL_SparkMax(0);
+        this._sparkLeft2 = new WL_SparkMax(0);
+        this._sparkLeft3 = new WL_SparkMax(0);
 
-        this.SparkRight1Master = new WL_SparkMax(0);
-        this.SparkRight2 = new WL_SparkMax(0);
-        this.SparkRight3 = new WL_SparkMax(0);
+        this._sparkRight1Master = new WL_SparkMax(0);
+        this._sparkRight2 = new WL_SparkMax(0);
+        this._sparkRight3 = new WL_SparkMax(0);
 
-        SparkLeft1Master.setFollowers(SparkLeft2, SparkLeft3);
-        SparkRight1Master.setFollowers(SparkRight2, SparkRight3);
+        _sparkLeft1Master.setFollowers(_sparkLeft2, _sparkLeft3);
+        _sparkRight1Master.setFollowers(_sparkRight2, _sparkRight3);
 
-        drive = new WL_DifferentialDrive( SparkLeft1Master, SparkRight1Master);
+        drive = new WL_DifferentialDrive(_sparkLeft1Master, _sparkRight1Master);
     }
 
     public void curvatureDrive(double throttle, double steering, boolean isQuickTurn) {
