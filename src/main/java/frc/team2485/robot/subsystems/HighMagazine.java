6658bd9d8c2176b787bd9161baff09fc7627b4c7
mark-rifkin <mrifkin2022@francisparker.org>
2020-02-11 21:28:37 -0800

    added subsystem javadoc

--- src/main/java/frc/team2485/robot/subsystems/HighMagazine.java
+++ src/main/java/frc/team2485/robot/subsystems/HighMagazine.java
@@ -13,14 +13,22 @@ public class HighMagazine extends SubsystemBase {
 
     private PIDTalonSRX m_talon;
 
-    private DigitalInput m_exitIR;
-
+    /**
+     * the number of balls currently contained in the high belt
+     */
     private int m_numBalls;
+
+    private DigitalInput m_exitIR;
 
     private BooleanSupplier m_transferIR;
 
     private boolean m_transferIRLastVal, m_exitIRLastVal;
 
+    /**
+     * High magazine subystem, controlling the top belt stage and outtake rollers.
+     * @param transferIR boolean supplier for the beam break sensor at the intersection of
+     *                   the low and high belts, provided through the LowMagazine subsystem.
+     */
     public HighMagazine(BooleanSupplier transferIR)  {
         m_talon = new PIDTalonSRX(Constants.Magazine.TALON_HIGH_PORT, ControlMode.Current);
 
@@ -28,33 +36,66 @@ public class HighMagazine extends SubsystemBase {
 
         m_numBalls = 0;
     }
+
+    /**
+     * Sets talon to a specific PWM
+     * @param pwm PWM to set the talon to
+     */
     public void setPWM(double pwm) {
         m_talon.set(pwm);
     }
 
+    /**
+     * Sets the talon to a specific current using PID control
+     * @param current current to set the PID to
+     */
     public void setCurrent(double current) {
         m_talon.runPID(current);
     }
+
+    /**
+     *
+     * @return belt encoder position
+     */
     public double getEncoderPosition() {
         return m_talon.getSensorCollection().getQuadraturePosition();
     }
 
+    /**
+     *
+     * @return belt encoder velocity
+     */
     public double getEncoderVelocity() {
         return Deadband.deadband(m_talon.getSensorCollection().getQuadraturePosition(), Constants.Magazine.ENCODER_VELOCITY_DEADBAND);
     }
 
+    /**
+     *
+     * @return value of beam break sensor at intersection of low and high belts
+     */
     public boolean getTransferIR() {
         return m_transferIR.getAsBoolean();
     }
 
+    /**
+     *
+     * @return boolean value of beam break sensor at end of high belt
+     */
     public boolean getExitIR() {
         return m_exitIR.get();
     }
 
+    /**
+     *
+     * @return boolean number of balls currently in the high belt
+     */
     public int getNumBalls() {
         return m_numBalls;
     }
 
+    /**
+     * Periodic method updating the number of balls in the high belt using beam break sensors.
+     */
     @Override
     public void periodic() {
 
--- src/main/java/frc/team2485/robot/subsystems/LowMagazine.java
+++ src/main/java/frc/team2485/robot/subsystems/LowMagazine.java
@@ -13,10 +13,16 @@ public class LowMagazine extends SubsystemBase {
 
     private DigitalInput m_entranceIR, m_transferIR;
 
+    /**
+     * the number of balls currently contained in the low belt
+     */
     private int m_numBalls;
 
     private boolean m_entranceIRLastVal, m_transferIRLastVal, m_exitIRLastVal;
 
+    /**
+     * Low magazine subystem, controlling the intake rollers and low belt.
+     */
     public LowMagazine() {
         m_talon = new PIDTalonSRX(Constants.Magazine.TALON_LOW_PORT, ControlMode.Current);
 
@@ -28,34 +34,65 @@ public class LowMagazine extends SubsystemBase {
         m_entranceIRLastVal = false;
     }
 
+    /**
+     * Sets talon to a specific PWM
+     * @param pwm PWM to set the talon to
+     */
     public void setPWM(double pwm) {
         m_talon.set(pwm);
     }
 
+    /**
+     * Sets the talon to a specific current using PID control
+     * @param current current to set the PID to
+     */
     public void setCurrent(double current) {
         m_talon.runPID(current);
     }
 
+    /**
+     *
+     * @return belt encoder position
+     */
     public double getEncoderPosition() {
-        return Deadband.deadband(m_talon.getSensorCollection().getQuadraturePosition(), Constants.Magazine.ENCODER_VELOCITY_DEADBAND);
-    }
-
-    public double getEncoderVelocity() {
         return m_talon.getSensorCollection().getQuadraturePosition();
     }
 
+    /**
+     *
+     * @return belt encoder velocity
+     */
+    public double getEncoderVelocity() {
+        return Deadband.deadband(m_talon.getSensorCollection().getQuadraturePosition(), Constants.Magazine.ENCODER_VELOCITY_DEADBAND);
+    }
+
+    /**
+     *
+     * @return boolean value of beam break sensor at start of low belt
+     */
     public boolean getEntranceIR() {
         return m_entranceIR.get();
     }
 
+    /**
+     *
+     * @return value of beam break sensor at intersection of low and high belts
+     */
     public boolean getTransferIR() {
         return m_transferIR.get();
     }
 
+    /**
+     *
+     * @return boolean number of balls currently in the high belt
+     */
     public int getNumBalls() {
         return m_numBalls;
     }
 
+    /**
+     * Periodic method updating the number of balls in the low belt using beam break sensors.
+     */
     @Override
     public void periodic() {
         if (!getEntranceIR() && getEntranceIR() != m_entranceIRLastVal) {
