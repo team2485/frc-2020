7c196eca10449022e656a463797ffe8a0d5b8146
Mark Rifkin <mrifkin2022@francisparker.org>
2021-10-01 09:11:15 -0700

    functional code, shooting setpoints

--- src/main/java/frc/team2485/robot/Constants.java
+++ src/main/java/frc/team2485/robot/Constants.java
@@ -82,7 +82,7 @@ public final class Constants {
     public static final class Setpoints {
         public static final Setpoint INITIATION_LINE = new Setpoint(-4500, 22);
         public static final Setpoint CLOSE_TRENCH = new Setpoint(-4500, 10);
-        public static final Setpoint FAR = new Setpoint(-5000, 10);
+        public static final Setpoint FAR = new Setpoint(-4500, 10);
 
         // public static final Setpoint GREEN_ZONE = new Setpoint(-5000, 27);
         // public static final Setpoint YELLOW_ZONE = new Setpoint(-5000, 16.9);
@@ -171,7 +171,7 @@ public final class Constants {
         public static final double HIGH_DISTANCE_PER_REVOLUTION = HIGH_GEAR_RATIO * 2 * Math.PI * ROLLER_RADIUS;
 
         public static final double LOW_INTAKE_BY_ONE_POS = -7;
-        public static final double HIGH_INDEX_BY_ONE_POS = -7.56;
+        public static final double HIGH_INDEX_BY_ONE_POS = -7.6;
         public static final double HIGH_INCREMENT_TOP = -3;
         public static final double PUSH_IN_INCREMENT = -3;
 
